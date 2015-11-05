2014aef4f9a88f97b320dba6abc4294e9ad5c840
bxshi <baoxu.shi@gmail.com>
2015-11-05 01:24:50 -0500

    fix dup yylval and yyktype

--- src/parser/main.c
+++ src/parser/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "parser.tab.h"
 #include "token.h"
 #include "decl.h"
 #include "expr.h"
--- src/parser/scanner.flex
+++ src/parser/scanner.flex
@@ -1,6 +1,5 @@
 %{
   #include "token.h"
-	#include "parser.tab.h"
 %}
 
 %{
