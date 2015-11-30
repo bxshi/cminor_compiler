24c647d01054d67784a0aa33976094fa8131bf45
bxshi <baoxu.shi@gmail.com>
2015-11-30 14:40:41 -0500

    remove old tests

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import subprocess
 # Compile library
 
 subprocess.call(["gcc", "-S", "./src/codegen/library.c", "-o", "library.s"])
+subprocess.call(["gcc", "-S", "./src/codegen/gfx.h", "./src/codegen/gfx.c", "-o", "gfx.s"])
 
 TEST_PATH = "./test/"
 
@@ -22,5 +23,8 @@ for fname in test_files:
 	subprocess.call(["./cminor", "-codegen", cminor_path, s_path])
 
 	bin_path = "".join([fname, ".out"])
-	subprocess.call(["gcc", "-s", s_path, "library.s", "-o", bin_path])
+	if fname == "bounce.out":
+		subprocess.call(["gcc", "-s", s_path, "library.s", "gfx.s", "-o", bin_path])
+	else:
+		subprocess.call(["gcc", "-s", s_path, "library.s", "-o", bin_path])
 
