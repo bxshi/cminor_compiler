35407a74435a9837e6f0d8b873c89cadfc668ddf
bxshi <baoxu.shi@gmail.com>
2015-12-01 11:23:39 -0500

    test cases

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ import subprocess
 # Compile library
 
 subprocess.call(["gcc", "-S", "./src/codegen/library.c", "-o", "library.s"])
-subprocess.call(["gcc", "-S", "./src/codegen/gfx.h", "./src/codegen/gfx.c", "-o", "gfx.s"])
 
 TEST_PATH = "./test/"
 
@@ -23,8 +22,12 @@ for fname in test_files:
 	subprocess.call(["./cminor", "-codegen", cminor_path, s_path])
 
 	bin_path = "".join([fname, ".out"])
-	if fname == "bounce.out":
-		subprocess.call(["gcc", "-s", s_path, "library.s", "gfx.s", "-o", bin_path])
+	subprocess.call(["gcc", "-s", s_path, "library.s", "-o", bin_path])
+
+	rtn = subprocess.call(["".join(["./",bin_path])])
+	if rtn == 0:
+		print " ".join(["\t\t\t",bin_path, "okay"])
 	else:
-		subprocess.call(["gcc", "-s", s_path, "library.s", "-o", bin_path])
+		print " ".join(["\t\t\t",bin_path, "returns",str(rtn)])
+
 
